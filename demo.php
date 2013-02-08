69f2bb52521ca091cce012739919e8a43933a12d
Léo Maradan <maradan.leo@gmail.com>
2013-02-08 09:38:54 +0100

    Petits ajustages + UTF8
    - Remplacement des double guillemets par des simples guillements
    - Ajout de guillements pour tout les index de tableau (Pour éviter les
    notices)
    - Passage en UTF-8

--- demo.php
+++ demo.php
@@ -1,4 +1,8 @@
 <?php
+
+// Force l'affichage en UTF-8
+header('Content-Type: text/html; charset=utf-8');
+
 include('syndexport.php');
 $xml = file_get_contents("rss.xml");
 $flux1 = new SyndExport($xml);
@@ -6,8 +10,8 @@ $type1 = $flux1->returnType();
 $nbr1 = $flux1->countItems();
 echo "Le flux1 d'actualités est de type : $type1, il possède $nbr1 entrée(s) dont voici la dernière :<br>";
 $entree1 = $flux1->exportItems(1);
-echo "Titre : ".$entree1[0][title]." <br>";
-echo "Description : ".$entree1[0][description]."<br>";
+echo "Titre : ".$entree1[0]['title']." <br>";
+echo "Description : ".$entree1[0]['description']."<br>";
 
 
 $atom = file_get_contents("atom.xml");
@@ -19,9 +23,9 @@ $liste=$flux2->exportItems();
 for($i=0;$i!=count($liste);$i++)
 {
 	$donnees=$liste[$i];
-	echo "<h2>$donnees[title]</h2>";
-	echo "<p>$donnees[description]</p>";
-	echo "<p>Ecrit le : $donnees[date]</p>";
-	echo "<a href=\"$donnees[link]\">$donnees[title]</a>";
+	echo "<h2>".$donnees['title']."</h2>";
+	echo "<p>".$donnees['description']."</p>";
+	echo "<p>Ecrit le : ".$donnees['date']."</p>";
+	echo "<a href=\"".$donnees['link']."\">".$donnees['title']."</a>";
 }
 ?>
